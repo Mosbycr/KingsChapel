e887ec13a4195c41c90320a1ea058e532fd3efaf
Mosbycr <mosbycr@gmail.com>
2019-08-27 21:35:19 -0400

    created Home page, added react router and home route to app.js, created navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // import './App.css';
+import Home from "../src/pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hellooo</p>
-    </div>
+    <Router>
+      <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+      </div>
+    </Router>
   );
 }
 
