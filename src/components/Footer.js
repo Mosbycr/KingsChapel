3511e08969e2924bc04be12750fcda4a1b42df64
Mosbycr <mosbycr@gmail.com>
2019-09-09 14:58:41 -0400

    added text-center to footer p tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
       <div className="container-fluid footer sticky-bottom">
         <div className="row">
           <div className="col-12">
-            <p className="copyright ml-auto mr-auto pt-3">copyright &copy; 2019 King's Chapel Episcopal Church</p>
+            <p className="copyright text-center pt-3">copyright &copy; 2019 King's Chapel Episcopal Church</p>
           </div>
         </div>
       </div>
