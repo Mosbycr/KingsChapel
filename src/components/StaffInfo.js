1bb2ae2151404bb78ebb0000b4182148031300d2
Mosbycr <mosbycr@gmail.com>
2019-09-09 15:26:08 -0400

    created staffinfo.js with bootstrap cards, added it into staff page

---
+++ src/components/StaffInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function StaffInfoCard(){
+    return (
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <p className="card-text">Title: </p>
+          <p className="card-text">Name: </p>
+          <p className="card-text">Contact: </p>
+        </div>
+      </div>
+    );
+}
+
+export default StaffInfoCard;
--- src/pages/Staff.js
+++ src/pages/Staff.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import StaffInfoCard from '../components/StaffInfo';
 
 function Staff(){
     return (
-        <div>
-            <p>This is the staff page</p>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-12 text-center">
+                    <h3>Staff Directory</h3>
+                </div>
+            </div>
+            <div className="row">
+                <StaffInfoCard />
+            </div>
         </div>
     )
 }
