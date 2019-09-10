170f753f74afef85ea0f5ed9483f549c389a016b
Mosbycr <mosbycr@gmail.com>
2019-09-10 19:43:13 -0400

    added detailed info to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,13 @@ function Footer(){
       <div className="container-fluid footer sticky-bottom">
         <div className="row text-center">
           <div className="col-4">
-            <p>King's Chapel Presbyterian Church<br/>
-            Address here
+            <p>
+              King's Chapel Presbyterian Church
+              <br />
+              13346 West Patrick Henry Road <br />
+              Doswell, Virginia 23047
             </p>
+            <p>Phone: 804-883-5831</p>
           </div>
           <div className="col-4">
             <p className="copyright text-center pt-3">
@@ -15,10 +19,11 @@ function Footer(){
             </p>
           </div>
           <div className="col-4">
-            <p>Phone: </p>
-            <p>Sunday Service Hours: <br/>
-            hours go here 
-{/* add any office hours maybe? */}
+            <p>
+              Sunday Worship Hours: <br />
+              9:45 a.m. Sunday School <br />
+              11:00 a.m. Sunday Worship
+              {/* add any office hours maybe? */}
             </p>
           </div>
         </div>
