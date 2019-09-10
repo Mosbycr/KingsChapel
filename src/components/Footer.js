01cf33fc2d3550ea7c4dfbc82f13bed4181b840e
Mosbycr <mosbycr@gmail.com>
2019-09-10 19:37:30 -0400

    added border top to footer, added color to body

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ function Footer(){
       <div className="container-fluid footer sticky-bottom">
         <div className="row text-center">
           <div className="col-4">
-            <p>King's Chapel Episcopal Church<br/>
+            <p>King's Chapel Presbyterian Church<br/>
             Address here
             </p>
           </div>
           <div className="col-4">
             <p className="copyright text-center pt-3">
-              &copy; 2019 King's Chapel Episcopal Church
+              &copy; 2019 King's Chapel Presbyterian Church
             </p>
           </div>
           <div className="col-4">
