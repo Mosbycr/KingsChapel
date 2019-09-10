88cb104db0736262967925465ba7a900a63c2e40
Mosbycr <mosbycr@gmail.com>
2019-09-10 10:55:02 -0400

    added bootstrap grid and content to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
 function Contact(){
-    return(
-        <div>
-            <p>This is the contact page</p>
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <h3>Contact Us</h3>
+          </div>
         </div>
-    )
+        <div className="row">
+          <div className="col-lg-12">
+            <p>
+              Give us a call, email us, or stop by on a Sunday. We would love to
+              hear from you!
+            </p>
+            <p>
+              Address:
+              <br />
+              King's Chapel Episcopal Church
+              <br />
+              Address info here
+            </p>
+            <p>
+              Phone:
+              <br />
+              phone number here
+            </p>
+            <p>
+              Email: <br />
+              email address here
+            </p>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Contact;
