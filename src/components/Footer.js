375739028f916d327da80344aa3bb412d2b60484
Mosbycr <mosbycr@gmail.com>
2019-09-11 15:49:57 -0400

    changed sunday school hours in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,29 +2,30 @@ import React from 'react';
 
 function Footer(){
     return (
-      <div className="container-fluid footer">
-        <div className="row text-center">
-          <div className="col-4">
-            <p>
-              King's Chapel Presbyterian Church
-              <br />
-              13346 West Patrick Henry Road <br />
-              Doswell, Virginia 23047
-            </p>
-            <p>Phone: 804-883-5831</p>
-          </div>
-          <div className="col-4">
-            <p className="copyright text-center pt-3">
-              &copy; 2019 King's Chapel Presbyterian Church
-            </p>
-          </div>
-          <div className="col-4">
-            <p>
-              Sunday Worship Hours: <br />
-              9:45 a.m. Sunday School <br />
-              11:00 a.m. Sunday Worship
-              {/* add any office hours maybe? */}
-            </p>
+      <div className="footerContainer">
+        <div className="container-fluid footer">
+          <div className="row text-center">
+            <div className="col-4">
+              <p>
+                King's Chapel Presbyterian Church
+                <br />
+                13346 West Patrick Henry Road <br />
+                Doswell, Virginia 23047
+              </p>
+              <p>Phone: 804-883-5831</p>
+            </div>
+            <div className="col-4">
+              <p className="copyright text-center pt-3">
+                &copy; 2019 King's Chapel Presbyterian Church
+              </p>
+            </div>
+            <div className="col-4">
+              <p>
+                Sunday Worship Hours: <br />
+                10:00 a.m. Sunday School <br />
+                11:00 a.m. Sunday Worship
+              </p>
+            </div>
           </div>
         </div>
       </div>
