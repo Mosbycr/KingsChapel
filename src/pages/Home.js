bffada2cde5ff4b8ea352257946e04154e671151
Mosbycr <mosbycr@gmail.com>
2019-09-18 08:25:25 -0400

    added church mission to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,20 +9,43 @@ function Home() {
         <div className="row">
           <div className="col-12"></div>
           <p className="mx-auto pt-3">
-            <em><b>
-              For where two or three are gathered in my name, I am there among
-              them. -Matthew 18:20
-           </b></em>
+            <em>
+              <b>
+                For where two or three are gathered in my name, I am there among
+                them. -Matthew 18:20
+              </b>
+            </em>
           </p>
         </div>
         <div className="row pt-3">
           <div className="col-lg-12">
-          <h3>Welcome</h3>
-          <p>Since 1930, King's Chapel Presbyterian Church has been a place for our community to gather
-            to share times of joy and sorrow, praise and worship and to form lasting Christian relationships.
-          King's Chapel strives to be a beacon of spiritual light for our community. We embrace Christ's timeless message of peace, hope, and love. We hope the light within
-            each of us shines as we witness for our Savior and guides us as we face the challenges of life.
-          </p>
+            <h3>Welcome</h3>
+            <p>
+              Since 1930, King's Chapel Presbyterian Church has been a place for
+              our community to gather to share times of joy and sorrow, praise
+              and worship and to form lasting Christian relationships. King's
+              Chapel strives to be a beacon of spiritual light for our
+              community. We embrace Christ's timeless message of peace, hope,
+              and love. We hope the light within each of us shines as we witness
+              for our Savior and guides us as we face the challenges of life.
+            </p>
+          </div>
+        </div>
+        <div className="row pt-3">
+          <div className="col-lg-12">
+            <h3>Our Mission</h3>
+            <h4>We are called to be a people of God.</h4>
+            <p>The Scriptures and the daily proof of God's grace form the basis of the call to be God's people.</p>
+            <h4>We are called to be a people of God's love.</h4>
+            <p>King's Chapel is a friendly church family that gathers for fellowship and spiritual growth.</p>
+            <h4>We are called to be a people acting on God's word.</h4>
+            <p>We strive to enrich those around us by:
+              <ul>
+                <li>showing God's love through personal example.</li>
+                <li>worshiping God as a congregation of faith.</li>
+                <li>participating in missions, both locally and around the world.</li>
+              </ul>
+            </p>
           </div>
         </div>
       </div>
