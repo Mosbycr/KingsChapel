c9013877058e0ad5e52af23d5d31dca101c10f29
Mosbycr <mosbycr@gmail.com>
2019-10-01 08:28:14 -0400

    added animated.css to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,12 @@ function Home() {
       <Images />
       <div className="container flexPadd">
         <div className="row pt-2">
-          <div className="col-lg-12">
+          <div className="col-lg-12 animated fadeIn delay-1s">
             <p className="pt-3" id="quote">
               For where two or three are gathered in my name, I am there among
               them. -Matthew 18:20
             </p>
-            <h3 className="pt-5 text-center">Welcome</h3>
+            <h3 className="pt-5 text-center animated tada delay-2s">Welcome</h3>
             <p className="pt-2 welcome">
               Since 1930, King's Chapel Presbyterian Church has been a place for
               our community to gather to share times of joy and sorrow, praise
@@ -25,7 +25,7 @@ function Home() {
           </div>
         </div>
         <div className="row pt-3">
-          <div className="col-lg-12 text-center">
+          <div className="col-lg-12 text-center animated fadeIn delay-3s">
             <h3>Our Mission</h3>
             <h5 className="pt-2">We are called to be a people of God:</h5>
             <p>
